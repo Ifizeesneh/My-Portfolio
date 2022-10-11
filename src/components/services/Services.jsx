4d5c74b336c0b63293f20677c1cbd5bcbe363d90
Ifeoluwa <deleoyeroifeoluwa@gmail.com>
2022-10-11 17:23:31 +0100

    offer

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,7 +5,7 @@ import './services.css'
 const Services = () => {
   return (
     <section id="services">
-      <h5>What i Offer</h5>
+      <h5>What I Offer</h5>
       <h2>Services</h2>
       <div className="container services_container">
         <article className="service">
