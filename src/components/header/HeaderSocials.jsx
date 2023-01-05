d93ac872621cb0c6b264a1dafda2266daac4de5b
Ifeoluwa <deleoyeroifeoluwa@gmail.com>
2023-01-05 12:58:22 +0100

    socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {FaTwitter} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="http://linkedin.com" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="http://github.com" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      <a href="http://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+      <a href="https://www.linkedin.com/in/ifeoluwa-dele-oyero-63411a223/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/Ifizeesneh" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      <a href="https://twitter.com/ifizeesneh" target="_blank" rel="noreferrer"><FaTwitter /></a>
     </div>
   )
 }
